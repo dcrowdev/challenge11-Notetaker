f4bdad1d72cacbfe4237778baf245226a4a1a2a6
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-29 16:57:30 -0400

    working functionality

--- routes/noteRouter.js
+++ routes/noteRouter.js
@@ -2,65 +2,52 @@ const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
 const fs = require('fs');
 const util = require('util')
-const notesArr = require('../db/db.json')
-
+// const notesArr = require('../db/db.json')
 const readFromFile = util.promisify(fs.readFile)
-
+const writeToFile = util.promisify(fs.writeFile)
 
 // Post request
 notes.post('/', (req, res) => {
-        console.info(`${req.method} request recieved to post a new note`)
+    console.info(`${req.method} request recieved to post a new note`)
+    readFromFile('./db/db.json', 'utf-8')
+        .then((data) => {
+            var notesArr = JSON.parse(data);
+            console.log(notesArr)
+            const { title, text } = req.body;
 
-        const note = req.body;
-
-        const newNote = {
-                title: req.body.title,
-                text: req.body.text,
-                note_id: uuid()
-            }
-
-        if (!note) {
-
-            return res.status(400).json({ msg: 'Please write a note'})
-        
-        } else {
-            notesArr.push(newNote)
-            res.json(notesArr)
-        }
-
-        // const noteString = JSON.stringify(newNote);
-        
-        // fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        // if(err) {
-        //   console.log(err);
-        // } else {
-        //     const parsedNotes = JSON.parse(data);
-        //     parsedNotes.push(newNote)
-        // }
-        // fs.appendFile('./db/db.json', noteString, (err) => err ? console.error(err) : console.log(`New note added!`))
-
-  });
+            const newNote = {
+                title,
+                text,
+                id: uuid()
+            }    
+            const newNoteArr = [...notesArr, newNote]
+            console.log(newNoteArr)
+            writeToFile('./db/db.json', JSON.stringify(newNoteArr)).then( () => res.json({ msg: 'OK' }))
+        });
+});
 
 // Get Request
 notes.get('/', (req, res) => {
     console.info(`${req.method} request recieved for notes`)
-    readFromFile('./db/db.json')
-        .then((data) => { 
+    readFromFile('./db/db.json', 'utf-8')
+        .then((data) => {
             res.json(JSON.parse(data))
-    });
+        });
 });
 
 // Delete request
-notes.delete('/:note_id', (req, res) => {
+notes.delete('/:id', (req, res) => {
     console.info(`${req.method} request recieved for notes`);
-    const noteFound = notesArr.some(note => note.note_id === parseInt(req.params.note_id));
+ 
+        readFromFile('./db/db.json', 'utf-8')
+        .then((data) => {
+            var oldNotes = [].concat(JSON.parse(data));
+            var updatedNotes = oldNotes.filter(note => note.id !== req.params.id)
+            writeToFile('./db/db.json', JSON.stringify(updatedNotes))
+            .then( () => res.json({ msg: 'OK' })).catch(err => res.status(400).json({ msg: `No note with the id ${req.params.id}` }))
+        });
 
-    if (noteFound) {
-        res.json(notesArr.filter(note => note.note_id !== parseInt(req.params.note_id)))
-    } else {
-        res.status(400).json({ msg: `No note with the id ${req.params.note_id}`})
-    }
-})
+});
 
 
 module.exports = notes;
